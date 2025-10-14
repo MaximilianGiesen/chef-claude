4da0822f7bde0940ef187763ddd89e656e71f9ba
MaximilianGcodes <maximilian.giesen@denkwerk.com>
2025-10-14 15:02:39 +0200

    Added Form component

---
+++ chef claude/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import icon from "../assets/chef-claude-icon.png"
+
+export default function Header() {
+    return (
+        <div className="header-sect">
+            <img src={icon} alt="chef-claude" />
+            <h1>Chef Claude</h1>
+        </div>
+    )
+}
