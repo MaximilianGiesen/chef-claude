c6561c28b4e94975abcb4fbbc028e13b9fa3d8eb
MaximilianGcodes <maximilian.giesen@denkwerk.com>
2025-10-14 15:59:39 +0200

    added basic clickhandler and mapping over ingredients

--- chef claude/src/components/Form.jsx
+++ chef claude/src/components/Form.jsx
@@ -1,14 +1,35 @@
 export default function Form() {
+    const ingredients = ["Chicken", "Oregano", "Tomatoes"]
+
+    const ingredientsListItems = ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
+
+    function addIngredient(e) {
+        e.preventDefault()
+        const formData = new FormData(e.currentTarget)
+        const newIngredient = formData.get("ingredient")
+
+        ingredients.push(newIngredient);
+        console.log(ingredients)
+    }
+
     return (
         <main>
-            <form className="add-ingredient-form">
+            <form className="add-ingredient-form" onSubmit={addIngredient}>
                 <input
                     type="text"
                     placeholder="e.g. oregano"
                     aria-label="Add ingredient"
+                    name="ingredient"
                 />
-                <button>Add ingredient</button>
+                <button type="submit">
+                    Add ingredient
+                </button>
             </form>
+            <ul>
+                {ingredientsListItems}
+            </ul>
         </main>
     )
 }
