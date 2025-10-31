bac94f1242de9d936b8613db327457e3be35c030
MaximilianGcodes <maximilian.giesen@denkwerk.com>
2025-10-31 08:16:24 +0100

    fix: Added alt text to logo

--- chef claude/src/components/Header.jsx
+++ chef claude/src/components/Header.jsx
@@ -3,7 +3,7 @@ import icon from "../assets/chef-claude-icon.png"
 export default function Header() {
     return (
         <header>
-            <img src={icon}/>
+            <img src={icon} alt="Chef Claude Logo"/>
             <h1>Chef Claude</h1>
         </header>
     )
